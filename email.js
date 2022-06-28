14c40a52ac4d1bb20a5bbba1bab3db9f4a19f456
sickCoder <lunagio683@gmail.com>
2022-06-27 20:28:09 -0700

    dark toggle for home and contact

--- email.js
+++ email.js
@@ -1,1 +1,12 @@
 // email 
+// same dark function
+function toggleDark(e){
+    
+    // very simple dark mode toggle for home
+    document.body.classList.toggle("text-bg-dark");
+
+    if(document.body.classList == "text-bg-dark"){
+        document.getElementsByTagName('svg').toggle('moonlight');
+    }
+
+}
--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 console.log(`wassup foo`);
 
-// work on email page form submission
+function toggleDark(e){
+    
+    // very simple dark mode toggle for home
+    document.body.classList.toggle("text-bg-dark");
+
+    if(document.body.classList == "text-bg-dark"){
+        document.getElementsByTagName('svg').toggle('moonlight');
+    }
+
+}
+
