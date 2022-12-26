dcf0f45936264eae4bcd865b3a0de9a05ec7bf54
sickCoder <lunagio683@gmail.com>
2022-12-26 15:15:14 -0800

    new components added.

---
+++ welcome/src/components/Links.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Links = () => {
+    return(
+        <div>
+            <ul>
+             <li><a href='https://github.com/'>Github</a></li>
+             <li></li>
+            </ul>
+        </div>
+    )
+}
+export default Links;
